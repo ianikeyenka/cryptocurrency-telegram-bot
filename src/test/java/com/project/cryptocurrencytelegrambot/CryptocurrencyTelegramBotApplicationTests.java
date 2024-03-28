2f877feaa49a65f68a3a5193f62e1c00fd770073
ianikeyenka <20prorok02@gmail.com>
2024-03-28 23:20:52 +0300

    Initial commit

---
+++ src/test/java/com/project/cryptocurrencytelegrambot/CryptocurrencyTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.cryptocurrencytelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptocurrencyTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
